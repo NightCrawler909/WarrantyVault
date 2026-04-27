7ec829cb810e188a29126dcf06c217cd28fe1fe1
NightCrawler909 <aayushsolanke@gmail.com>
2026-04-27 09:25:00 +0530

    vercel fix

--- client/components/common/SmoothScroll.tsx
+++ client/components/common/SmoothScroll.tsx
@@ -39,8 +39,9 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
             smoothWheel: true,
             wheelMultiplier: 1,
             touchMultiplier: 2,
+            // @ts-ignore
             normalizeWheel: true,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+            easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
           }
         });
       } catch (error) {
